0d9c32f790bf106b9dc06c39b5655d07b95f49b9
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-06-30 21:13:39 -0700

    refactor: simplify DNS record names

--- dns.tf
+++ dns.tf
@@ -9,21 +9,17 @@ resource "cloudflare_zone" "main" {
 resource "cloudflare_dns_record" "www" {
   comment = "Created during Cloudflare Rules deployment process for Redirect from WWW to root"
   content = "192.0.2.1"
-  name    = "www.${var.server_domain}"
+  name    = "www"
   proxied = true
   ttl     = 1
   type    = "A"
   zone_id = cloudflare_zone.main.id
 }
 
-locals {
-  cloudflare_tunnel_url = "${cloudflare_zero_trust_tunnel_cloudflared.main.id}.cfargotunnel.com"
-}
-
 resource "cloudflare_dns_record" "tunnels" {
   for_each = toset(["@", "webdav", "vault", "rss", "status", "auth"])
-  content  = local.cloudflare_tunnel_url
-  name     = each.value == "@" ? var.server_domain : "${each.value}.${var.server_domain}"
+  content  = "${cloudflare_zero_trust_tunnel_cloudflared.main.id}.cfargotunnel.com"
+  name     = each.value
   proxied  = true
   ttl      = 1
   type     = "CNAME"
@@ -32,7 +28,7 @@ resource "cloudflare_dns_record" "tunnels" {
 
 resource "cloudflare_dns_record" "ssh" {
   content = oci_core_instance.main.public_ip
-  name    = "ssh.${var.server_domain}"
+  name    = "ssh"
   proxied = false
   ttl     = 1
   type    = "A"
@@ -50,7 +46,7 @@ resource "cloudflare_dns_record" "mail" {
 
 resource "cloudflare_dns_record" "dkim" {
   content = data.oci_email_dkim.main.cname_record_value
-  name    = "${data.oci_email_dkim.main.name}._domainkey.${var.server_domain}"
+  name    = "${data.oci_email_dkim.main.name}._domainkey"
   proxied = false
   ttl     = 1
   type    = "CNAME"
@@ -60,7 +56,7 @@ resource "cloudflare_dns_record" "dkim" {
 # https://docs.oracle.com/en-us/iaas/Content/Email/Tasks/configurespf.htm
 resource "cloudflare_dns_record" "spf" {
   content = "\"v=spf1 include:rp.oracleemaildelivery.com ~all\""
-  name    = var.server_domain
+  name    = "@"
   proxied = false
   ttl     = 1
   type    = "TXT"
